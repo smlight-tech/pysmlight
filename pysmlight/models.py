80aa12564eb0a6be2263e4a16cf936e9c7b0118c
Tim Lunn <tl@smlight.tech>
2024-03-10 22:26:49 +1100

    Update models with most data fields reqd from API

--- pysmlight/models.py
+++ pysmlight/models.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+
 @dataclass
 class Firmware:
     mode:str
@@ -22,26 +23,41 @@ class Firmware:
 
 @dataclass
 class Info:
+    coord_mode: str
+    device_ip: str
+    fw_channel: str # NEW - beta or stable (based on installed esp firmware)
     MAC: str
     model: str
     sw_version:str
     zb_hw: str
     zb_version: str
+    zb_type: str # NEW - should be enum (coordinator, router, thread?)
 
     def __init__(self, payload):
+        self.coord_mode = payload.mode
         self.MAC = payload.MAC
         self.model = payload.model
         self.sw_version = payload.sw_version
         self.zb_hw = payload.json['zbHw']
         self.zb_version = payload.zb_version
 
-
 @dataclass
 class Sensors:
     esp32_temp: float
     zb_temp: float
     mode: str
-    uptime: str
+    uptime: str # Should be timestamp
+    socket_uptime: str  # Should be timestamp
+    ram_usage: int
+    internet: bool
+    ethernet: bool
+    wifi_connected: bool
+    wifi_status: str # Should be enum (off, client, AP etc)
+
+    #toggle states:
+    disable_led: bool
+    night_mode: bool
+    auto_zigbee: bool
 
     def __init__(self, payload):
         self.esp32_temp = payload.esp32_temp
