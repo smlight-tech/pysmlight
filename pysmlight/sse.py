6dc04b6130ef4506d96a7d7a2e3e970672e2815b
Tim Lunn <tl@smlight.tech>
2024-03-13 12:23:16 +1100

    Add 30sec timeout on socket reads
    This will allow reconnect if no PING message is received within 30secs

--- pysmlight/sse.py
+++ pysmlight/sse.py
@@ -1,5 +1,7 @@
 import logging
 from aiohttp_sse_client2 import client as sse_client
+import aiohttp
+import asyncio
 from typing import Callable
 from .const import Events
 
@@ -17,15 +19,25 @@ class sseClient:
         self.url = f"http://{host}/events"
 
     async def client(self):
-        async with sse_client.EventSource(
-            self.url, session=self.session, timeout=None
-        ) as event_source:
-            try:
-                async for event in event_source:
-                    _LOGGER.debug(event)
-                    await self.message_handler(event)
-            except ConnectionError:
-                _LOGGER.debug("Connection error")
+        timeout = aiohttp.ClientTimeout(total=None, connect=None, sock_connect=None, sock_read=30)
+        while True:
+            async with sse_client.EventSource(
+                self.url, session=self.session, timeout=timeout
+            ) as event_source:
+                try:
+                    async for event in event_source:
+                        _LOGGER.debug(event)
+                        await self.message_handler(event)
+                except asyncio.exceptions.CancelledError:
+                    _LOGGER.debug('async cancelled')
+                except aiohttp.ClientConnectionError:
+                    _LOGGER.debug("Client Connection error")
+                except aiohttp.ClientError:
+                    _LOGGER.debug("Client error")
+                except ConnectionError:
+                    _LOGGER.debug("Connection error")
+                else:
+                    _LOGGER.debug("Connection closed cleanly")
 
     async def message_handler(self, event):
         self.callbacks.get(event.type, lambda x: None)(event)
@@ -33,7 +45,7 @@ class sseClient:
 
     def register_callback(self, event:Events, cb: Callable):
         """ register a callback for a specific event type or all events"""
-        if event.name in self.callbacks:
+        if event and event.name in self.callbacks:
             _LOGGER.warning(f"Callback for {event} already exists, overwriting")
 
         if event is not None:
