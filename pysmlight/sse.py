19ab633d014e4d88f16dd5cb086791ab9ac9b237
Tim Lunn <tl@smlight.tech>
2024-08-30 11:40:50 +1000

    Update docstrings and type hints for SSE

--- pysmlight/sse.py
+++ pysmlight/sse.py
@@ -1,3 +1,4 @@
+"""Client to receive Server Sent Events (SSE) from the SMLIGHT API."""
 import json
 import logging
 from typing import Callable
@@ -19,6 +20,7 @@ class sseClient:
     """Initialise a client to receive Server Sent Events (SSE)"""
 
     def __init__(self, host: str, session: aiohttp.ClientSession):
+        """Initialise the SSE client."""
         self.callbacks: dict[Events, Callable] = {}
         self.settings_cb: dict[Settings, Callable] = {}
         self.session = session
@@ -29,29 +31,31 @@ class sseClient:
 
         self.register_callback(Events.SAVE_PARAMS, self._handle_settings)
 
-    async def client(self):
+    async def client(self) -> None:
         while True:
             await self.sse_stream()
 
-    async def sse_stream(self):
+    async def sse_stream(self) -> None:
+        """Process incoming events on the message stream"""
         async with EventSource(
             self.url, session=self.session, timeout=self.timeout
         ) as event_source:
             try:
                 async for event in event_source:
                     _LOGGER.debug(event)
-                    await self.message_handler(event)
+                    await self._message_handler(event)
             except ConnectionError:
                 _LOGGER.debug("Client Connection error")
             else:
                 _LOGGER.debug("Connection closed cleanly")
 
-    async def message_handler(self, event: MessageEvent):
+    async def _message_handler(self, event: MessageEvent) -> None:
+        """Match event with callback for event type"""
         self.callbacks.get(getattr(Events, event.type), lambda x: None)(event)
         self.callbacks.get(Events.CATCH_ALL, lambda x: None)(event)
 
     def register_callback(self, event: Events, cb: Callable) -> Callable[[], None]:
-        """register a callback for a specific event type or all events"""
+        """register a callback for a specific event type"""
         if event and event in self.callbacks:
             _LOGGER.warning("Callback for %s already exists, overwriting", event)
 
@@ -63,11 +67,12 @@ class sseClient:
         return remove_callback
 
     def deregister_callback(self, event: Events) -> None:
-        """Deregister callbacks per event type"""
+        """Deregister callback for event type"""
         if event in self.callbacks:
             del self.callbacks[event]
 
     def _handle_settings(self, event: Events) -> None:
+        """Process event and match callback for settings changes"""
         data = json.loads(event.data)
         page = Pages(data["page"])
         changes = data.pop("changes", None)
@@ -90,6 +95,7 @@ class sseClient:
     def register_settings_cb(
         self, setting: Settings, cb: Callable
     ) -> Callable[[], None]:
+        """Register a callback for a specific setting"""
         self.settings_cb[setting] = cb
 
         def remove_callback():
@@ -98,5 +104,6 @@ class sseClient:
         return remove_callback
 
     def deregister_settings_cb(self, setting: Settings) -> None:
+        """Deregister callback for a setting"""
         if setting in self.settings_cb:
             del self.settings_cb[setting]
