4b96ac652a957ef54c19347e0e6f08b630e5829b
Tim Lunn <tl@smlight.tech>
2024-08-30 21:13:25 +1000

    test with async

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,8 +3,8 @@ from aiohttp import ClientSession
 from aresponses import ResponsesMockServer
 import pytest
 
-from pysmlight import Api2
 from pysmlight.exceptions import SmlightAuthError
+from pysmlight.web import Api2, webClient
 
 host = "slzb-06.local"
 USER = "admin"
@@ -61,3 +61,20 @@ async def test_auth_is_success(aresponses: ResponsesMockServer) -> None:
     async with ClientSession() as session:
         client = Api2(host, session=session)
         assert await client.authenticate(USER, PASSWORD) is True
+
+
+async def test_with_async(aresponses: ResponsesMockServer) -> None:
+    """Test that authentication has access the API."""
+    aresponses.add(
+        host,
+        "/api2",
+        "GET",
+        aresponses.Response(
+            status=200,
+            headers={"Content-Type": "application/json"},
+        ),
+        repeat=2,
+    )
+    async with webClient(host) as client:
+        assert client.session is not None
+        assert await client.check_auth_needed() is False
