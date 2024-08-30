d21f3961d403f5a6d2fc815874850d588c996850
Tim Lunn <tl@smlight.tech>
2024-08-30 20:46:15 +1000

    improve test_auth

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -6,8 +6,6 @@ import pytest
 from pysmlight import Api2
 from pysmlight.exceptions import SmlightAuthError
 
-from . import load_fixture
-
 host = "slzb-06.local"
 USER = "admin"
 PASSWORD = "admin"
@@ -22,7 +20,6 @@ async def test_auth_not_needed(aresponses: ResponsesMockServer) -> None:
         aresponses.Response(
             status=200,
             headers={"Content-Type": "application/json"},
-            text=load_fixture("slzb-06-info.json"),
         ),
     )
     async with ClientSession() as session:
@@ -41,26 +38,11 @@ async def test_auth_is_required(aresponses: ResponsesMockServer) -> None:
             headers={"Content-Type": "application/json"},
             text="wrong login or password",
         ),
+        repeat=2,
     )
     async with ClientSession() as session:
         client = Api2(host, session=session)
         assert await client.check_auth_needed() is True
-
-
-async def test_auth_is_failed(aresponses: ResponsesMockServer) -> None:
-    """Test that authentication has failed to access the API."""
-    aresponses.add(
-        host,
-        "/api2",
-        "GET",
-        aresponses.Response(
-            status=401,
-            headers={"Content-Type": "application/json"},
-            text="wrong login or password",
-        ),
-    )
-    async with ClientSession() as session:
-        client = Api2(host, session=session)
         with pytest.raises(SmlightAuthError):
             await client.authenticate(USER, PASSWORD)
 
