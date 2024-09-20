ea555a6093c468cd38e3a6e41b703a10c5bef167
Tim Lunn <tl@smlight.tech>
2024-09-20 22:17:20 +1000

    test settings post errors

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -3,9 +3,11 @@ import urllib
 
 from aiohttp import ClientSession
 from aresponses import ResponsesMockServer
+import pytest
 
 from pysmlight import Api2
 from pysmlight.const import Settings
+from pysmlight.exceptions import SmlightAuthError, SmlightConnectionError, SmlightError
 
 host = "slzb-06.local"
 
@@ -36,3 +38,27 @@ async def test_settings_toggle(aresponses: ResponsesMockServer) -> None:
         assert bool(body["ha"][0]) is True
         value = True if body[key][0] in "on" else False
         assert value
+
+
+@pytest.mark.parametrize(
+    ("code", "exception"), [(401, SmlightAuthError), (404, SmlightConnectionError)]
+)
+async def test_settings_toggle_post_error(
+    aresponses: ResponsesMockServer, code: int, exception: SmlightError
+) -> None:
+    """Test toggling settings on SLZB devices."""
+    aresponses.add(
+        host,
+        "/settings/saveParams",
+        "POST",
+        aresponses.Response(
+            status=code,
+            headers={"Content-Type": "application/json"},
+            text="ok",
+        ),
+    )
+    async with ClientSession() as session:
+        client = Api2(host, session=session)
+        with pytest.raises(exception):
+            page, key = Settings.DISABLE_LEDS.value
+            await client.set_toggle(page, key, True)
