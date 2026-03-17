e810b92845fc523cc5e9d312e4fa22cd410e90c2
Tim Lunn <tl@smlight.tech>
2026-03-17 13:03:19 +1100

    convert info and sensor tests to use snapshots

--- tests/test_info.py
+++ tests/test_info.py
@@ -7,8 +7,9 @@ from aiohttp import ClientSession
 from aiohttp.client_exceptions import ClientConnectionError
 from aresponses import ResponsesMockServer
 import pytest
+from syrupy.assertion import SnapshotAssertion
 
-from pysmlight import Api2, Info, Radio
+from pysmlight import Api2, Info
 from pysmlight.const import Settings
 from pysmlight.exceptions import SmlightAuthError, SmlightConnectionError
 
@@ -17,7 +18,9 @@ from . import load_fixture
 host = "slzb-06.local"
 
 
-async def test_info_device_info(aresponses: ResponsesMockServer) -> None:
+async def test_info_device_info(
+    aresponses: ResponsesMockServer, snapshot: SnapshotAssertion
+) -> None:
     """Test getting SLZB device information."""
     aresponses.add(
         host,
@@ -32,39 +35,12 @@ async def test_info_device_info(aresponses: ResponsesMockServer) -> None:
     async with ClientSession() as session:
         client = Api2(host, session=session)
         info: Info = await client.get_info()
-        assert info
-
-        assert info.wifi_mode == 0
-        assert info.ram_total == 285
-        assert info.fs_total == 3456
-        assert info.zb_ram_size == 296
-        assert info.zb_flash_size == 1024
-        assert info.coord_mode == 0
-        assert info.device_ip == "192.168.1.63"
-        assert info.fw_channel == "dev"
-        assert info.MAC == "DD:88:FC:AA:EE:FF"
-        assert info.model == "SLZB-06p10"
-        assert info.sw_version == "v2.5.2"
-        assert info.zb_hw == "CC2674P10"
-        assert info.zb_version == "20240315"
-        assert info.zb_type == 0
-        assert info.legacy_api == 0
-        assert info.hostname == "SLZB-06P10"
-        assert len(info.radios) == 1
-        assert info.radios is not None
-        assert info.radios[0] == Radio(
-            chip_index=0,
-            zb_channel=None,
-            zb_flash_size=1024,
-            zb_hw="CC2674P10",
-            zb_ram_size=296,
-            zb_version="20240315",
-            zb_type=0,
-            radioModes=None,
-        )
+        assert info == snapshot
 
 
-async def test_info_device_mr_info(aresponses: ResponsesMockServer) -> None:
+async def test_info_device_mr_info(
+    aresponses: ResponsesMockServer, snapshot: SnapshotAssertion
+) -> None:
     """Test getting SLZB device information for multi radios."""
     aresponses.add(
         host,
@@ -79,42 +55,27 @@ async def test_info_device_mr_info(aresponses: ResponsesMockServer) -> None:
     async with ClientSession() as session:
         client = Api2(host, session=session)
         info: Info = await client.get_info()
-        assert info
+        assert info == snapshot
 
-        assert info.wifi_mode == 0
-        assert info.ram_total == 300
-        assert info.fs_total == 3456
-        assert info.coord_mode == 0
-        assert info.device_ip == "192.168.1.62"
-        assert info.fw_channel == "dev"
-        assert info.MAC == "DD:88:FC:AA:EE:FF"
-        assert info.model == "SLZB-MR1"
-        assert info.sw_version == "v2.7.1"
-        assert info.legacy_api == 0
-        assert info.hostname == "SLZB-MR1"
 
-        assert info.radios is not None
-        assert len(info.radios) == 2
-        assert info.radios[0] == Radio(
-            chip_index=0,
-            zb_channel=1,
-            zb_flash_size=768,
-            zb_hw="EFR32MG21",
-            zb_ram_size=96,
-            zb_version="20240510",
-            zb_type=0,
-            radioModes=[True, True, True, False, False],
-        )
-        assert info.radios[1] == Radio(
-            chip_index=1,
-            zb_channel=1,
-            zb_flash_size=704,
-            zb_hw="CC2652P7",
-            zb_ram_size=152,
-            zb_version="20240716",
-            zb_type=0,
-            radioModes=[True, True, True, False, False],
-        )
+async def test_ultima_info(
+    aresponses: ResponsesMockServer, snapshot: SnapshotAssertion
+) -> None:
+    """Test getting SLZB Ultima device information."""
+    aresponses.add(
+        host,
+        "/ha_info",
+        "GET",
+        aresponses.Response(
+            status=200,
+            headers={"Content-Type": "application/json"},
+            text=load_fixture("slzb-ultima-info.json"),
+        ),
+    )
+    async with ClientSession() as session:
+        client = Api2(host, session=session)
+        info: Info = await client.get_info()
+        assert info == snapshot
 
 
 async def test_info_get_auth_fail(aresponses: ResponsesMockServer) -> None:
@@ -169,7 +130,9 @@ async def test_info_post_connector_error(mock_post) -> None:
             await client.set_toggle(_page, _toggle, True)
 
 
-async def test_info_sensors(aresponses: ResponsesMockServer) -> None:
+async def test_info_sensors(
+    aresponses: ResponsesMockServer, snapshot: SnapshotAssertion
+) -> None:
     """Test getting SLZB sensor data."""
     aresponses.add(
         host,
@@ -184,20 +147,27 @@ async def test_info_sensors(aresponses: ResponsesMockServer) -> None:
     async with ClientSession() as session:
         client = Api2(host, session=session)
         sensors = await client.get_sensors()
-        assert sensors
+        assert sensors == snapshot
 
-        assert sensors.esp32_temp == 39.44
-        assert sensors.zb_temp == 35.76
-        assert sensors.zb_temp2 == 34.20
-        assert sensors.uptime == 700
-        assert sensors.socket_uptime is None
-        assert sensors.ram_usage == 91
-        assert sensors.fs_used == 192
-        assert sensors.ethernet is True
-        assert sensors.wifi_connected is False
-        assert sensors.wifi_status == 2
-        assert sensors.disable_leds is False
-        assert sensors.night_mode is False
+
+async def test_ultima_sensors(
+    aresponses: ResponsesMockServer, snapshot: SnapshotAssertion
+) -> None:
+    """Test getting SLZB Ultima sensor data."""
+    aresponses.add(
+        host,
+        "/ha_sensors",
+        "GET",
+        aresponses.Response(
+            status=200,
+            headers={"Content-Type": "application/json"},
+            text=load_fixture("slzb-ultima-sensors.json"),
+        ),
+    )
+    async with ClientSession() as session:
+        client = Api2(host, session=session)
+        sensors = await client.get_sensors()
+        assert sensors == snapshot
         assert sensors.auto_zigbee is False
 
 
@@ -227,14 +197,8 @@ async def test_info_legacy_info(aresponses: ResponsesMockServer) -> None:
         info: Info = await client.get_info()
         assert info
 
-        assert info.device_ip == "192.168.1.157"
-        assert info.fw_channel == "release"
-        assert info.MAC == "DD:88:FC:AA:EE:FF"
-        assert info.model == "SLZB-06p10"
         assert info.sw_version == "v2.0.20"
-        assert info.zb_hw == "CC2674P10"
         assert info.zb_version is None
-        assert info.zb_channel == 2
         assert info.legacy_api == 1
 
 
@@ -267,9 +231,6 @@ async def test_info_legacy_info2(aresponses: ResponsesMockServer) -> None:
         info: Info = await client.get_info()
         assert info
 
-        assert info.device_ip == "192.168.1.157"
-        assert info.MAC == "DD:88:FC:AA:EE:FF"
-        assert info.model == "SLZB-06"
         assert info.sw_version == "0.9.9"
         assert info.legacy_api == 2
 
